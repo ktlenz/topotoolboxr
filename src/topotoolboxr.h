cfb2d3a2e9763b73bd2355a75705435cfea5d9ea
Domenic Bartha <72920179+FancyBirb@users.noreply.github.com>
2025-01-07 10:11:42 +0100

    Fillsink (#16)
    Co-authored-by: William Kearney <william.kearney@uni-potsdam.de>

--- src/topotoolboxr.h
+++ src/topotoolboxr.h
@@ -5,5 +5,6 @@ void wrap_has_topotoolbox(int *a);
 
 void wrap_gradient8(float *outputR,float *demR, float *cellsizeR,int *use_mpR, int* dimsR);
 
+void wrap_fillsink(float *output, float *dem, int *bcR, int *dimsR);
 
 #endif // TOPOTOOLBOXR_H
