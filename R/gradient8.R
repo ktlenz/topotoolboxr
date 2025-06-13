ee3c13b685bb6c1205a1ae3cdb83abff9ad7568d
Kilian Lenz <132825721+ktlenz@users.noreply.github.com>
2025-06-13 12:12:23 +0200

    Feature: Added missing value handling, unit conversion and unit tests for gradient8 (#28)
    Closes #24
    
    Missing value handling
    
        gradient8.R now allows passing missing data into the libtopotoolbox subroutine
    
    Unit conversion
    
        Unit conversions from tangent to degree, radian, sine or percent were previously unimplemented
        Conversions are now carried out in the gradient8.R script
    
    Tests
    
        New tests regarding missing data and unit conversions implemented
        New and existing tests pass

--- R/gradient8.R
+++ R/gradient8.R
@@ -1,23 +1,60 @@
 #' wrap_gradient8
 #'
-#' This will make the Gradient8 funktion available to R from the libtotopotoolbox subdirectory
-#' 
-#' @param dem Input of type Spatrast from Terra
-#' @param use_mp Future feature that will allow to parallelize the code
+#' Computes the gradient of a digital elevation model (DEM) using an 8-direction algorithm,
+#' making the Gradient8 function available to R from the libtotopotoolbox subdirectory.
+#'
+#' @param dem Digital elevation model (SpatRaster, terra)
+#' @param unit Unit of returned gradient values. Options are:
+#'   - 'tangent': Calculate the gradient as a tangent (default).
+#'   - 'radian': Calculate the gradient in radians.
+#'   - 'degree': Calculate the gradient in degrees.
+#'   - 'sine': Calculate the gradient as the sine of the angle.
+#'   - 'percent': Calculate the gradient as a percentage.
+#' @param use_mp Logical. If TRUE, use parallel processing for computation (future feature).
+#'   Currently not implemented.
 #'
 #' @import terra
 #'
-#' @return A Spatrast
+#' @return 8-connected neighborhood gradient of a digital elevation model (SpatRaster)
+#' 
+#' @examples
+#' \dontrun{
+#' DEM <- terra::rast(system.file("ex/elev.tif",package="terra"))
+#' DEM <- terra::project(DEM,"epsg:32632",res=90.0)
+#' g <- gradient8(DEM)
+#' plot(g)
+#' }
+#' 
 #' @export
 
-gradient8 <- function(dem,use_mp=0) {
+gradient8 <- function(dem,unit='tangent',use_mp=0) {
+    # Extract input data
+    d <- get_grid_data(dem)
     
-    d <- get_grid_data(dem) # Extract input data
-    output <- single(length(d$z)) #create output array
-    result <- .C("wrap_gradient8",outputR=as.single(output),as.single(d$z),as.single(d$cellsize),as.integer(use_mp), as.integer(d$dims))$outputR
-
-    G <- dem # copy Spatrast
-    terra::values(G) <- result #update Spatrast values
+    # Compute gradient8 using libtopotoolbox
+    output <- single(length(d$z))
+    result <- .C("wrap_gradient8",
+                 outputR=as.single(output),
+                 as.single(d$z),as.single(d$cellsize),
+                 as.integer(use_mp),
+                 as.integer(d$dims),
+                 NAOK=TRUE)$outputR
+    result[is.na(d$z)] <- NA
     
-    return(G)
+    # Unit conversion
+    if (unit == 'degree'){
+      result <- atan(result) * 180 / pi
+    } else if (unit == 'radian') {
+      result <- atan(result)
+    } else if (unit == 'sine') {
+      result <- sin(atan(result))
+    } else if (unit == 'percent'){
+      result <- result*100
+    }
+    
+    # Store results as terra SpatRaster
+    g <- dem
+    terra::values(g) <- result
+    
+    return(g)
 }
